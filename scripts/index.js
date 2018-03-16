be8d62c8148683de63f828b16a9105530f11d5a1
Daniel Medrano <bestenglishking@hotmail.com>
2018-03-16 00:15:38 -0600

    Cambios importantes

--- scripts/index.js
+++ scripts/index.js
@@ -1,112 +1,146 @@
 (function() {
+	var result = $('#result').hide(),
+		loading = $('#loading').hide(),
+		err = $('#error').hide(),
+		resultList = $('#result-list').hide(),
+		span = $('span'),
+		table = $('table');
 
-var result = $('#result').hide(),
-	loading = $('#loading').hide(),
-	resultList = $('#result-list').hide(),
-	span = $('span');
+	/*
+	 * ########## onclick listeners ##########
+	 */
 
-$('#throw-dice').click(function(e) {
-	e.preventDefault();
-	resultList.show();
+	$('#throw-dice').click(function(e) {
+		e.preventDefault();
+		resultList.show();
 
-	result.hide();
-	loading.show();
-	getRandInts(getMin(), getMax());
-});
-
-$('#erase-list').click(function(e) {
-	resultList.html('');
-	resultList.hide();
-});
-
-function getRandInts(min, max) {
-	// API Key: 3a1b27f9-9547-44a3-abf7-71ceb007f68e
-	var rndId = Math.floor((Math.random() * 10000)),
-		o = {
-		'jsonrpc': '2.0',
-		'method': 'generateIntegers',
-		'params': {
-			'apiKey': '3a1b27f9-9547-44a3-abf7-71ceb007f68e',
-			'n': 1,
-			'min': min,
-			'max': max
-		},
-		'id': rndId
-	};
-
-	$.ajax({
-		'url': 'https://api.random.org/json-rpc/1/invoke',
-		'type': 'POST',
-		'contentType': 'application/json-rpc',
-		'data': JSON.stringify(o)
-	})
-	.done(function(data) {
-		console.log(data);
-
-		if (rndId == data.id) {
-			var rdata = data.result.random.data[0];
-			span.text(rdata + ' (' + defRange(rdata) + ')');
-			resultList.prepend(rdata + ' (' + defRange(rdata) + ')<br>');
-		}
-		loading.hide();
-		result.show();
+		result.hide();
+		err.hide();
+		loading.show();
+		getRandInts(getMin(), getMax());
 	});
 
-}
+	$('#erase-list').click(function(e) {
+		resultList.html('');
+		resultList.hide();
+	});
 
-function getMin() {
-	var min = 0;
+	$('#ad').click(function(e) {
+		e.preventDefault();
 
-	min = parseInt(document.getElementById('min0').value);
-	if (parseInt(document.getElementById('min1').value) < min)
-		min = parseInt(document.getElementById('min1').value);
-	if (parseInt(document.getElementById('min2').value) < min)
-		min = parseInt(document.getElementById('min2').value);
-	if (parseInt(document.getElementById('min3').value) < min)
-		min = parseInt(document.getElementById('min3').value);
+		table.append($('<tr><td><input type=\"text\" placeholder=\"Nombre\"></td><td><input type=\"number\" id=\"min\" min=\"0\" max=\"200\" placeholder=\"Mínimo\"> - <input type=\"number\" id=\"max\" min=\"0\" max=\"200\" placeholder=\"Máximo\"></td></tr>'));
+	});
 
-	return min;
-}
+	$('#as').click(function(e) {
+		e.preventDefault();
 
-function getMax() {
-	var max = 0;
+		var o = table.find('tr'),
+			i = o.length;
 
-	max = parseInt(document.getElementById('max0').value);
-	if (parseInt(document.getElementById('max1').value) > max)
-		max = parseInt(document.getElementById('max1').value);
-	if (parseInt(document.getElementById('max2').value) > max)
-		max = parseInt(document.getElementById('max2').value);
-	if (parseInt(document.getElementById('max3').value) > max)
-		max = parseInt(document.getElementById('max3').value);
+		if (i > 2)
+			o[i - 1].remove();
+	});
 
-	return max;
-}
+	/*
+	 * ########## api function ##########
+	 */
 
-function defRange(val) {
-	var array = [],
-		str = '';
+	function getRandInts(min, max) {
+		// API Key: 3a1b27f9-9547-44a3-abf7-71ceb007f68e
+		var rndId = Math.floor((Math.random() * 10000)),
+			o = {
+			'jsonrpc': '2.0',
+			'method': 'generateIntegers',
+			'params': {
+				'apiKey': '3a1b27f9-9547-44a3-abf7-71ceb007f68e',
+				'n': 1,
+				'min': min,
+				'max': max
+			},
+			'id': rndId
+		};
 
-	if (val <= parseInt(document.getElementById('max0').value) && val >= parseInt(document.getElementById('min0').value))
-		array.push('Brandon');
-	if (val <= parseInt(document.getElementById('max1').value) && val >= parseInt(document.getElementById('min1').value))
-		array.push('Daniel');
-	if (val <= parseInt(document.getElementById('max2').value) && val >= parseInt(document.getElementById('min2').value))
-		array.push('Ingrid');
-	if (val <= parseInt(document.getElementById('max3').value) && val >= parseInt(document.getElementById('min3').value))
-		array.push('Ulises');
+		$.ajax({
+			'url': 'https://api.random.org/json-rpc/1/invoke',
+			'type': 'POST',
+			'contentType': 'application/json-rpc',
+			'data': JSON.stringify(o)
+		})
+		.done(function(data) {
+			if (rndId == data.id && data.error == null) {
+				var rdata = data.result.random.data[0];
+				span.text(rdata + ' (' + defRange(rdata) + ')');
+				resultList.prepend(rdata + ' (' + defRange(rdata) + ')<br>');
 
-	// Format array as string
-	for (i = 0; i < array.length; i++) {
-		if ((i + 1) == array.length) {
-			str += array[i];
-		} else if ((i + 2) == array.length) {
-			str += array[i] + ' y ';
-		} else {
-			str += array[i] + ', ';
-		}
+				result.show();
+			} else {
+				err.show();
+				console.log(data.error);
+			}
+
+			loading.hide();
+		});
 	}
 
-	return str;
-}
+	/*
+	 * ########## helper functions ##########
+	 */
 
+	function getMin() {
+		var min = 0,
+			num;
+
+		$.each(table.find('tr'), function(i, v) {
+			num = parseInt($(v).find('#min')[0].value);
+
+			if (i == 0)
+				min = num;
+			else if (num < min)
+				min = num;
+		});
+
+		return min;
+	}
+
+	function getMax() {
+		var max = 0,
+			num;
+
+		$.each(table.find('tr'), function(i, v) {
+			num = parseInt($(v).find('#max')[0].value);
+
+			if (i == 0)
+				max = num;
+			else if (num > max)
+				max = num;
+		});
+
+		return max;
+	}
+
+	function defRange(val) {
+		var array = [], str = '',
+			min, max, name;
+
+		$.each(table.find('tr'), function(i, v) {
+			min = parseInt($(v).find('#min')[0].value);
+			max = parseInt($(v).find('#max')[0].value);
+			name = $(v).find('input')[0].value;
+
+			if (val <= max && val >= min)
+				array.push(name);
+		});
+
+		// Format array as string
+		for (i = 0; i < array.length; i++) {
+			if ((i + 1) == array.length)
+				str += array[i];
+			else if ((i + 2) == array.length)
+				str += array[i] + ' y ';
+			else
+				str += array[i] + ', ';
+		}
+
+		return str;
+	}
 })();
